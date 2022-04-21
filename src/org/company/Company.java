4c8a4c415f4eb9fd0df78c641f6d40699b96ee51
VigneshS02121994 <vigneshitvijay@gmail.com>
2022-04-21 21:39:08 +0530

    added

---
+++ src/org/company/Company.java
@@ -0,0 +1,17 @@
+package org.company;
+
+import org.client.Client;
+
+public class Company extends Client {
+	public void companyname() {
+		System.out.println("company name is Rajsriya");
+		}
+	public static void main(String[] args) {
+		Company l = new Company();
+		l.companyname();
+		l.clientname();
+		
+		
+	}
+
+}
